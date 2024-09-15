858d0d9e0bd9943e3a7331e57a9e2b2ee7f19518
huynhnhinguyen0301 <149548625+NguyenThiHuynhNhi-JP@users.noreply.github.com>
2024-09-15 18:09:44 +0700

    done

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
 ﻿#region Tính số ngày trong tuần và số lẻ
-// Console.WriteLine("Nhập số ngày:");
-// int soNgay = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine($"Số ngày vừa nhập là: {soNgay}");
+Console.WriteLine("Nhập số ngày:");
+int soNgay = Convert.ToInt32(Console.ReadLine());
 
-// int soTuan = soNgay / 7;
-// int ngayDu = soNgay % 7;
+int soTuan = soNgay / 7;
+int ngayDu = soNgay % 7;
 
-// Console.WriteLine($"Số Tuần : {soTuan}, Số ngày dư là: {ngayDu}");
+Console.WriteLine($"Số Tuần : {soTuan}, Số ngày dư là: {ngayDu}");
 #endregion
 
 #region Tính tổng giá trị đơn hàng sau khi áp dụng giảm giá
@@ -31,4 +30,63 @@ int soTienSale = giaTriBill / 100 * phanTramSale;
 int soTienThanhToan = giaTriBill - soTienSale;
 
 Console.WriteLine($"Giá trị đơn hàng: {giaTriBill}000 vnđ, Phần trăm giảm giá: {phanTramSale}%, Số tiền giảm: {soTienSale}000 vnđ, Số tiền phải thanh toán: {soTienThanhToan}000 vnđ.");
+#endregion
+
+#region Chuyển đổi thời gian từ giờ sang phút
+Console.WriteLine("Nhập vào số phút: ");
+int soPhut = Convert.ToInt32(Console.ReadLine());
+if (soPhut <= 0)
+{
+  Console.WriteLine("Số phút không thể nhỏ hơn 0.");
+  return;
+}
+int soGio = soPhut / 60;
+int soPhutDu = soPhut % 60;
+
+Console.WriteLine($"số phút nhập vào là: {soPhut} => {soGio} giờ, {soPhutDu} phút.");
+#endregion
+
+#region Tính tổng số tiền sau khi cộng thêm thuế VAT.
+Console.WriteLine("Nhập vào số tiền gốc vnđ (đơn vị: nghìn): ");
+int tienGoc = Convert.ToInt32(Console.ReadLine());
+if (tienGoc <= 0)
+{
+  Console.WriteLine("Tiền gốc không thể nhỏ hơn 0.");
+  return;
+}
+
+Console.WriteLine("Nhập vào tỉ lệ (%) thuế VAT: ");
+int tiLeThue = Convert.ToInt32(Console.ReadLine());
+if (tiLeThue < 0)
+{
+  Console.WriteLine("Tỉ lệ phải lớn hơn 0%.");
+  return;
+}
+
+int soTienThue = tienGoc / 100 * tiLeThue;
+int tongTien = tienGoc + soTienThue;
+
+Console.WriteLine($"Tiền gốc là: {tienGoc}000 vnđ, tỉ lệ thuế là: {tiLeThue}%, tiền thuế là: {soTienThue}000 vnđ, tổng tiền là: {tongTien}000 vnđ");
+#endregion
+
+#region Chuyển đổi tiền tệ USD => VNĐ
+Console.WriteLine("Nhập vào USD: ");
+int tienUSD = Convert.ToInt32(Console.ReadLine());
+if (tienUSD <= 0)
+{
+  Console.WriteLine("USD không thể nhỏ hơn 0.");
+  return;
+}
+
+Console.WriteLine("Nhập vào tỉ giá (%) chuyển đổi: ");
+int tiGia = Convert.ToInt32(Console.ReadLine());
+if (tiGia < 0)
+{
+  Console.WriteLine("Tỉ giá phải lớn hơn 0%.");
+  return;
+}
+
+int tienTuongUng = tienUSD * tiGia;
+
+Console.WriteLine($"Tiền tương ứng là: {tienTuongUng} vnđ");
 #endregion
--- obj/Debug/net8.0/btvn_buoi2.AssemblyInfo.cs
+++ obj/Debug/net8.0/btvn_buoi2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("btvn_buoi2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b355698f728bc16bd20c7aab6768b38417421cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+493af20e5b4227e26229250e3d161c8475c6b708")]
 [assembly: System.Reflection.AssemblyProductAttribute("btvn_buoi2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("btvn_buoi2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
